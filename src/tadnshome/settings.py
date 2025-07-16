7f97422a2ca81c271e0b5c417ca66df9dc217341
Aditya-Pratap-Bose <tadns999@gmail.com>
2025-07-16 15:53:15 +0530

    Updated build args

--- src/tadnshome/settings.py
+++ src/tadnshome/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = config("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower() == "true"
 DEBUG = config("DJANGO_DEBUG", cast=bool)
-
-print("DEBUG", DEBUG, type(DEBUG))
+# print("DEBUG", DEBUG, type(DEBUG))
 
 ALLOWED_HOSTS = [
     ".railway.app"  # https://saas.prod.railway.app
@@ -95,7 +94,7 @@ DATABASES = {
 }
 
 CONN_MAX_AGE = config("CONN_MAX_AGE", cast=int, default=30)
-DATABASE_URL = config("DATABASE_URL", default=None, cast=str)
+DATABASE_URL = config("DATABASE_URL", default=None, default=None)
 
 if DATABASE_URL is not None:
     import dj_database_url
